05a68277f479ecbbff010002129eefc11fcba4dd
Shereif10 <shereifelswaf7@gmail.com>
2026-06-17 17:02:35 +0300

    finish

--- src/app/[locale]/about/page.tsx
+++ src/app/[locale]/about/page.tsx
@@ -166,9 +166,9 @@ export default function AboutPage() {
         <VisionMissionSection />
         <WhyChooseSection />
         <LegacyProjectsSection />
-        <LegacyProjectsSection />
+        
         <ApproachSection />
-        <TestimonialSection />
+        
         <JourneyCta />
       </main>
 
